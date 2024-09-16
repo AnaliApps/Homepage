a26d442b72b1c08adcd754db85f186af397cd9f3
AnaliApps <ronoanali5@gmail.com>
2024-09-16 22:15:27 +0300

    Add styling for header and footer
    Added styling for header and footer.
    Added media queries to make it responsive.

--- src/views/footer.js
+++ src/views/footer.js
@@ -5,19 +5,30 @@ import icon_6000 from './images/icon_6000.jpg'
 export default function createFooter(){
     let footer = document.createElement("footer");
     footer.innerHTML = `
-    <div>
         <div>
-            <h1>Contact Me</h1>
-            <p>Please get in touch if you thank our work could e mutually beneficial.</p>
-            <p>1234 Random Road</p>
-            <p>Random Town, California 12345</p>
-            <p><span><img loading="lazy" src="./images/phone.svg" alt="Phone Icon></span>555-555-555</p>
-            <p><span><img loading="lazy" src="./images/email.svg" alt="Email Icon></span>notrealemail@gmail.com</p>
+            <div>
+                <h1 class="footer_header">Contact Me</h1>
+                <p>Please get in touch if you thank our work could e mutually beneficial.</p>
+                <p>1234 Random Road</p>
+                <p>Random Town, California 12345</p>
+                <p>555-555-555</p>
+                <p>notreal@gmail.com</p>
+                <ul>
+                <li>
+                <i class="devicon-github-original"></i>
+                </li>
+                <li>
+                    <i class="devicon-linkedin-plain"></i>
+                </li>
+                <li>
+                    <i class="devicon-twitter-original"></i>
+                </li>
+                </ul>
+            </div>
         </div>
         <div>
             <img loading="lazy" src=${icon_640} srcset="${icon_640} 640w,${icon_1280} 1280w,${icon_1920} 1920w,${icon_6000} 6000w" alt="Anali Rono" sizes="(max-width:800px) 100vw, 50vw" decoding="async" fetchpriority="high">
         </div>
-    </div>
     `
     return footer;
 }
--- src/views/header.js
+++ src/views/header.js
@@ -9,22 +9,22 @@ export default function headerContent(){
     <div class="icon_div">
         <img loading="lazy" src=${icon_640} srcset="${icon_640} 640w,${icon_1280} 1280w,${icon_1920} 1920w,${icon_6000} 6000w" alt="Anali Rono" sizes="(max-width:800px) 100vw, 50vw" decoding="async" fetchpriority="high">
     </div>
-    <div>
+    <div class="bio">
         <div>
-            <h1>About Me</h1>
+            <h1 class="bio-h1">About Me</h1>
             <p>
                 I am a skilled web designer specializing in creating dynamic and visually engaging websites using HTML, CSS, and JavaScript. With a strong focus on modern design principles and user experience, I leverage Webpack as my package manager to streamline and optimize the development process. My expertise ensures that websites are not only aesthetically pleasing but also functional and efficient.
             </p>
         </div>
-        <div>
+        <div class = "social_links>
             <ul class="links">
-                <li>
+                <li class="link">
             <i class="devicon-github-original"></i>
           </li>
-                <li>
+                <li class="link">
             <i class="devicon-linkedin-plain"></i>
           </li>
-                <li>
+                <li class="link">
             <i class="devicon-twitter-original"></i>
           </li>
             </ul>
